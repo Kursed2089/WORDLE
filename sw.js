b9989683aa52b9fa5b98779a96657413d7eaa57e
Kursed2089 <harryjhutton12@gmail.com>
2026-07-05 19:58:53 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,23 @@
-const CACHE_NAME = "wordle-uk-v1";
-const ASSETS = [
-  "/WORDLE/",
-  "/WORDLE/index.html"
+// sw.js
+const CACHE_NAME = 'v1';
+const URLS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/app.js',
+  '/icons/icon-192.png'
 ];
 
-self.addEventListener("install", (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(URLS_TO_CACHE))
   );
 });
 
-self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
     })
   );
 });
